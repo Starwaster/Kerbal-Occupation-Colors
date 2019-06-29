220b11c3ae7fe7a9e27b40e975ee6407ce6872c0
Starwaster <starwaster@gmail.com>
2019-06-29 15:37:18 -0400

    First file publishing
    * Version 1 of Kerbal Occupation Colors. Kerbals by default are assigned red for pilots, gold for engineers and blue for scientists when wearing the Future Suit from the Serenity DLC.

---
+++ KerbalOccupationColors.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using KSP;
+
+namespace KerbalOccupationColors
+{
+    [KSPAddon(KSPAddon.Startup.FlightEditorAndKSC, false)]
+    public class KerbalOccupationColors : MonoBehaviour
+    {
+        public KerbalOccupationColors()
+        {            
+        }
+
+        public void Start()
+        {
+            GameEvents.onKerbalAdded.Add(OnKerbalAdded);
+        }
+
+        void OnKerbalAdded(ProtoCrewMember kerbal)
+        {
+            Vector3 occupationColor = GetOccupationColor(kerbal.trait);
+            kerbal.lightR = occupationColor[0];
+            kerbal.lightG = occupationColor[1];
+            kerbal.lightB = occupationColor[2];
+        }
+
+        // KERBAL_OCCUPATION_COLORS
+        // {
+        //     pilot
+        //     {
+        //         red = 1
+        //     }
+        // }
+        public Vector3 GetOccupationColor(string occupation)
+        {
+            ConfigNode occupationColors = GameDatabase.Instance.GetConfigNodes("KERBAL_OCCUPATION_COLORS").Last();
+            if (occupationColors != null)
+            {
+                if (occupationColors.HasNode(occupation.ToLower()))
+                {
+                    ConfigNode colors = occupationColors.GetNode(occupation.ToLower());
+                    float red = 0f;
+                    float green = 0f;
+                    float blue = 0f;
+
+                    if (colors.HasValue("red"))
+                        float.TryParse(colors.GetValue("red"), out red);
+                    if (colors.HasValue("green"))
+                        float.TryParse(colors.GetValue("green"), out green);
+                    if (colors.HasValue("blue"))
+                        float.TryParse(colors.GetValue("blue"), out blue);
+
+                    return new Vector3(red, green, blue);
+                }
+                Debug.Log("[KerbalOccupationColors] occupation " + occupation + " not found in KERBAL_OCCUPATION_COLORS");
+            }
+            else
+                Debug.Log("[KerbalOccupationColors] KERBAL_OCCUPATION_COLORS config not found");
+            return new Vector3(1f, 0.5176f, 0f);
+        }
+    }
+}
